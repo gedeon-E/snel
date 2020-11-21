ff35b13b9dfb0d71b7762e0d6b4ae42b8bfffec9
gedeon <gedebamba3@gmail.com>
2020-11-21 10:24:40 +0100

    perform page payement

--- src/app/pages/payment/payment.page.ts
+++ src/app/pages/payment/payment.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentPage implements OnInit {
 
-  constructor() { }
+  constructor(private loadingController : LoadingController, 
+    private router : Router) { }
 
   ngOnInit() {
   }
 
+  async presentLoading() {
+    const loading = await this.loadingController.create({
+      cssClass: 'my-custom-class',
+      message: 'Veuillez patienter...',
+      duration: 2000
+    });
+    await loading.present();
+
+    // const { role, data } = await loading.onDidDismiss();
+    console.log('Loading dismissed!');
+    await loading.onDidDismiss().then(
+      (value) =>{
+        const { role, data } = value
+        this.router.navigate(['/home'])
+      }
+    );
+  }
 }
